3c68b362d6a32f9ab6d0c554afec23f01cb32eaa
samiatoui <atoui79@gmail.com>
2022-12-21 22:02:35 -0600

    big changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
-import './App.css';
-import Repolist from "./components/repolist/Repolist"
-import UserInfo from './components/userinfo/UserInfo';
+import './App.css'
+import React, { useState, useEffect } from 'react';
+import GitHub from "./github.svg"
+import External from "./external.svg";
+
 function App() {
+
+  const [username, setUser] = useState();
+
+  const getInput = (event) => {
+    const userValue = event.target.value;
+    setUser(userValue);
+
+  }
+
+
+  const [name, setName] = useState('');
+  const [img, setImg] = useState();
+  const [blog, setBlog] = useState('');
+  const [location, setLocation] = useState('');
+  const [bio, setBio] = useState('');
+
+  const [repos, setRepo] = useState([]);
+
+  const changeUser = async () => {
+    const results = await fetch(`https://api.github.com/users/${username}`).then((response => response.json()));
+    console.log(results.name);
+    setName(results.name);
+    setImg(results.avatar_url);
+    setBlog(results.blog);
+    setLocation(results.location);
+    setBio(results.bio);
+
+    const repoData = await fetch(`https://api.github.com/users/${username}/repos`);
+    const json = await repoData.json();
+    setRepo(json);
+  }
+
+
+
   return (
-    <div className="App">
-      <UserInfo/>
-      <Repolist/>
+    <div>
+
+
+      <div className='form'>
+        <input
+          type="text"
+          onChange={getInput}>
+        </input>
+        <button onClick={changeUser}>Search</button>
+      </div>
+      <div className="App">
+
+
+
+
+        <div className='user-info'>
+          <div className='user-img'>
+            <img src={img}></img>
+          </div>
+
+          <div className='user-bio'>
+            <h2>{name}</h2>
+            <p>{bio}</p>
+            <p>{location}</p>
+            <a href={blog} target="_blank"><img src={GitHub} className="github-icon"></img></a>
+          </div>
+        </div>
+        <div className="repo-main">
+          {
+            repos.map((repo) => (
+              <div className="repo-cont">
+                <div className="repo-item" key={repo}>
+                  <h2>{repo.name}</h2>
+                  <div className="repo-btncont">
+                    <div className="repo-button"><a href={repo.html_url} target="_blank"><img src={GitHub} title="View on GitHub" className="github-icon-repo"></img></a></div>
+                    {
+                      repo.homepage !== "" && repo.homepage !== null ? (<div className="repo-button"><a href={repo.homepage} target="_blank"><img src={External} title="View Live Demo" className="external-icon"></img></a>  </div>) : <div className="repo-button"></div>
+                    }
+                  </div>
+                  <p>{repo.description}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
